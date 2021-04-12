28dfd5dc97bbee709a4d3f642e64884634414351
taliayit <34692076+taliayit@users.noreply.github.com>
2021-04-12 14:26:09 +0300

    add search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,11 @@
-const SearchBar = ({ keyword, setKeyword }) => {
+function SearchBar({ input, onInputChange }) {
     const BarStyling = { width: "100%", background: "#F2F1F9", border: "none", padding: "0.5rem" };
+
     return ( <
         input style = { BarStyling }
-        value = { keyword }
+        value = { input }
         placeholder = { "Search actor..." }
-        onChange = { e => setKeyword(e.target.value) }
+        onChange = { e => onInputChange(e.target.value) }
         />
     );
 }
--- src/pages/ActorsPage.jsx
+++ src/pages/ActorsPage.jsx
@@ -5,19 +5,29 @@ import ActorCard from "../components/ActorCard";
 import SearchBar from "../components/SearchBar";
 
 function ActorsPage(props) {
-    const [actors, setActors] = useState([
+    var actorsData = [
         new ActorModel("Julia", "Roberts", "1967-10-28", "https://m.media-amazon.com/images/M/MV5BMTQzNjU3MDczN15BMl5BanBnXkFtZTYwNzY2Njc4._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000210/?ref_=nmls_hd"),
         new ActorModel("George", "Clooney", "1961-05-06", "https://m.media-amazon.com/images/M/MV5BMjEyMTEyOTQ0MV5BMl5BanBnXkFtZTcwNzU3NTMzNw@@._V1_UY317_CR9,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000123/?ref_=nmls_pst"),
         new ActorModel("Leonardo", "DiCaprio", "1974-11-11", "https://m.media-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY317_CR10,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000138/?ref_=nmls_pst"),
         new ActorModel("Will", "Smith", "1968-09-25", "https://m.media-amazon.com/images/M/MV5BNTczMzk1MjU1MV5BMl5BanBnXkFtZTcwNDk2MzAyMg@@._V1_UY317_CR2,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000226/?ref_=nmls_pst")
+    ];
 
-    ]);
+    const [input, setInput] = useState('');
+    const [actors, setActors] = useState(actorsData);
+
+    const updateInput = (input) => {
+        const filtered = actorsData.filter(actor => {
+            return (actor.fname.toLowerCase().includes(input.toLowerCase())) || (actor.lname.toLowerCase().includes(input.toLowerCase()));
+        });
+        setInput(input);
+        setActors(filtered);
+    }
 
     const actorsCards = actors.map(actor => <ActorCard actor={actor}/>);
-
+    
     return (
         <Container className="p-actors">
-            <SearchBar/>
+            <SearchBar input={input} onInputChange={updateInput}/>
             <div className="row">
                 {actorsCards}
             </div>
